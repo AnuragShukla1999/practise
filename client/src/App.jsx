dc4b96f02a8e459e2b302ccf9993c7df5830c442
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-02-22 20:23:51 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+
 
 const App = () => {
   return (
-    <div>App</div>
+     <BrowserRouter>
+        <Routes>
+           <Route path='/' element={<Home/>} />
+           <Route path='/about' element={<About/>} />
+           <Route path='/signup' element={<SignUp/>} />
+           <Route path='/signin' element={<SignIn/>} />
+        </Routes>
+     </BrowserRouter>
   )
 }
 
